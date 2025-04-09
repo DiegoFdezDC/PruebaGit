d53364ec2cbef08b1f31474f0901588f7375cc9b
DiegoFdezDC <diego.fernandezd@edu.uah.es>
2025-04-09 17:24:41 +0200

    Update KclientCS.py

--- P13/KclientCS.py
+++ P13/KclientCS.py
@@ -3,9 +3,11 @@ import select
 import sys
 import random
 
-IP = "127.0.0.1"
-PUERTO = 8080
 
+IP = f"127.0.0.{str(random.randint(1,200))}"
+PUERTO = 9999
+
+conver = []
 ID = str(random.randint(1, 1000))
 
 # Creamos el socket TCP y nos conectamos al servidor
@@ -15,8 +17,16 @@ socket_client.connect((IP, PUERTO))
 # Enviamos el identificador
 socket_client.send(ID.encode())
 
-name = input("¿Cual es tu nombre?")
-print(f"\nAhora otrs usuarios te veran como :{name}. Escribe exit para desconectarte\n")
+socket_client.send(name.encode())
+repe = socket_client.recv(1024).decode()
+if repe != "0":
+    name = name + repe
+
+
+
+name = input("[APP]¿Cual es tu nombre?\n")
+print("[APP] nombre registrado con exito, escribe help para ver mas comandos")
+print(f"\n[APP] BIen venido al chat grupal {name} \n")
 
 while True:
 
@@ -28,18 +38,37 @@ while True:
             # Mensaje recibido del servidor
             response = socket_client.recv(1024).decode()
             if not response:
+                
                 # Si el mensaje está vacío, el servidor cerró la conexión
-                print("\nEl servidor ha cerrado la conexión.")
+                print("\n[APP] El servidor ha cerrado la conexión.")
                 socket_client.close()
                 sys.exit()
             print(response)
+            conver.append(response)
+            
         else:
             # El usuario ha escrito algo y pulsado ENTER
             msg = sys.stdin.readline()  # como input(), pero no bloquea
-            if msg == "exit":
-                print("\nCerrando cliente.")
+            if msg == "exit\n":
+                print("\n[APP]Cerrando cliente.")
                 socket_client.close()
                 sys.exit()
+                
+            elif msg == "save\n":
+                with open(f"Chat_{name}.txt","w") as archivo:
+                    archivo.write(f"Conversacion de {name}\n")
+                    for coms in conver:#
+                        archivo.write(f"{coms}")
+                    
+                print("[APP] Archivo guardado con exito")
+                print("==================================")
+                
+            elif msg == "help\n"
+                print("[APP] Escribe exit para desconectarte")
+                print("[APP] Escribe save para guardar la conversacion")
+                
+
             message = "[" + name + "]:" +msg
+            conver.append(message)
             # Enviamos el mensaje al servidor
             socket_client.send(message.encode())
