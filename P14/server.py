33e20848d508179c891fb14a72d94a624cf58497
DiegoFdezDC <diego.fernandezd@edu.uah.es>
2025-04-21 12:57:41 +0200

    Update server.py

--- P14/server.py
+++ P14/server.py
@@ -1,14 +1,13 @@
 import socket
 import select
 
-
 PORT = 9999
-IP = "0.0.0.0" #dijo Luis que mejor en la de broadcast
+IP = "0.0.0.0"  # dijo Luis que mejor en la de broadcast
 IDS = 0
 
-#recuerda poner en los decode y encode "uft-8"
+# Recuerda poner en los decode y encode "utf-8"
 
-#Creamos socket TCP
+# Creamos socket TCP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # permite reutilizar el IP y puerto
 server_socket.bind((IP, PORT))
@@ -22,44 +21,56 @@ ID_collection = [IDS]  # Guarda los ID
 # Bucle principal
 while True:
     listo_leer, _, _ = select.select(sockets, [], [])  # uso del select para ir usando los que estén disponibles
-    
+
     for sock in listo_leer:
-		
+
         # Aquí vemos si alguien intenta iniciar sesión
         if sock == server_socket:
 
-            #Aceptamos conexion y guardamos el socket en la lista de sockets
+            # Aceptamos conexión y guardamos el socket en la lista de sockets
             client_socket, client_addr = server_socket.accept()
             sockets.append(client_socket)
 
             ID = client_socket.recv(1024).decode()
-            
-            #Agregamos el id a la coleccion
+
+            # Agregamos el id a la colección
             ID_collection.append(ID)
 
-            print(f"\n[SERVER] Se conecto un usuario con ID:{ID} desde {client_addr}")
+            print(f"\n[SERVER] SE conectó un usuario con ID:{ID} desde {client_addr}")
             print(f"Usuarios Conectados: {ID_collection}")
-        #Ahora vemos si hay clientes intentando mandar mensajes
+
+        # Ahora vemos si hay clientes intentando mandar mensajes
         else:
             try:
-                #Obtenemos el mensaje
+                # Obtenemos el mensaje
                 response = sock.recv(1024).decode()
                 indice = sockets.index(sock)
-                
-                # si se manda una lista vacia interpretamos que se desconecto
+
+                # Si se manda una lista vacía interpretamos que se desconectó
                 if not response:
-                    print(f"\n[SERVER] USUARIO: {ID_collection[indice]} se desconecto")
-                    # es mejor usar remove pq el pop tendríamos que recorrer las listas en busca de 1 igual
+                    print(f"\n[SERVER] USUARIO: {ID_collection[indice]} se desconectó")
                     sockets.pop(indice)
                     ID_collection.pop(indice)
                     sock.close()
-                    
-				#El mensaje lo reenviamos a todos menos al servidor y al que lo mando
-                else:
+
+                # El mensaje lo reenviamos a todos menos al servidor y al que lo mandó
+                elif response.startswith("["):
                     for cliente in sockets:
                         if cliente != server_socket and cliente != sock:
                             cliente.send(f"\n{response}".encode())
+                # Si no empieza con [ es que es un mensaje privado
+                else:
+                    busca = response.split(" ", 2)  # Ajustamos el split para que divida correctamente
+                    destinatario = busca[1]
+                    mensaje_privado = busca[2]
 
-            except:
-                print("Hubo un error, terminando el programa")
+                    print(f"Buscando {destinatario}...")
+
+                    if destinatario in ID_collection:
+                        indice_destinatario = ID_collection.index(destinatario)
+                        sockets[indice_destinatario].send(f"[Privado de {ID_collection[indice]}]: {mensaje_privado}".encode())
+
+            except Exception as e:
+                print(f"Hubo un error: {e}, terminando el programa")
                 exit()
+
